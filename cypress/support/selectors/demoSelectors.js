60ac494b803e70c58b3fdadd5083966a902c41fa
subhekshya Baral <baralshubu@gmail.com>
2024-08-23 12:29:42 +0545

    edit assertions in demo

---
+++ cypress/support/selectors/demoSelectors.js
@@ -0,0 +1,26 @@
+export const demoSelectors = { 
+    // login Selectors
+    userNameSelector: ('input[name="user-name"]'),
+    passwordSelector: ('input[name="password"]'),
+    loginButtonSelector: ('input[name="login-button"]'),
+
+    // burger menu button selector
+    burgerMenuButtonSelector: ('#react-burger-menu-btn'),
+
+    //add to cart selectors
+    addToCartSelector : ('#shopping_cart_container'),
+
+     // add to dropdown selector
+    dropdownSelector : ('select[data-test="product-sort-container"]'),
+
+    //items selectors
+    productNameSelector : ('div[data-test="inventory-item-name"]'),
+    ProductImageSelector : ('#item_4_title_link'),
+    productDescribeSelector : ('div[data-test="inventory-item-desc"]'),
+    productPriceSelector : ('div[data-test="inventory-item-price"]'),
+    addToCartSelector : ('#add-to-cart-sauce-labs-backpack'),
+
+    //logout Selector
+    logOutSelector : ('#logout_sidebar_link')
+
+}
