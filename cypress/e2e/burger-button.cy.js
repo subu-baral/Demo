b12c5ef87e421a98deb52801a5be6781e1175e3e
subhekshya Baral <baralshubu@gmail.com>
2024-08-26 08:07:32 +0545

    Add Selectors in burger menu button

--- cypress/e2e/burger-button.cy.js
+++ cypress/e2e/burger-button.cy.js
@@ -1,22 +1,24 @@
+import{demoSelectors}from "../support/selectors/demoSelectors"
+import { burgerButtonSelectors } from "../support/selectors/burgerButtonSelector";
 describe('Product page', () => {
   it('product page should be visible', () => {
     cy.visit('https://www.saucedemo.com/');
     cy.url().should('eq', 'https://www.saucedemo.com/');
     cy.contains('Swag Labs');
-    cy.get('#user-name').clear().type("standard_user");
-    cy.get('#password').clear().type("secret_sauce");
-    cy.get('#login-button').click();
+    cy.get(demoSelectors.userNameSelector).clear().type("standard_user");
+    cy.get(demoSelectors.passwordSelector).clear().type("secret_sauce");
+    cy.get(demoSelectors.loginButtonSelector).click();
     cy.url().should('include','/inventory.html');
 
-    cy.get('#react-burger-menu-btn').should('be.visible').and('be.enabled').click();
+    cy.get(demoSelectors.burgerMenuSelector).should('be.visible').and('be.enabled').click();
    
-    cy.get('#inventory_sidebar_link').should('be.visible').and('have.text', 'All Items').and('have.attr', 'href', '#');
+    cy.get(burgerButtonSelectors.allItemsSelector).should('be.visible').and('have.text', 'All Items').and('have.attr', 'href', '#');
     
-    cy.get('#about_sidebar_link').should('be.visible').and('have.text','About').and('have.attr','href','https://saucelabs.com/');
+    cy.get(burgerButtonSelectors.aboutSelector).should('be.visible').and('have.text','About').and('have.attr','href','https://saucelabs.com/');
     
-    cy.get('#logout_sidebar_link').should('be.visible').and('have.text','Logout').and('have.attr', 'href', '#');
+    cy.get(demoSelectors.logOutSelector).should('be.visible').and('have.text','Logout').and('have.attr', 'href', '#');
 
-    cy.get('#reset_sidebar_link').should('be.visible').and('have.text','Reset App State').and('have.attr', 'href', '#');
+    cy.get(burgerButtonSelectors.resetSelector).should('be.visible').and('have.text','Reset App State').and('have.attr', 'href', '#');
     
     cy.get ('body',{timeout:3000}).type('{esc}');
 
---
+++ cypress/support/selectors/burgerButtonSelector.js
@@ -0,0 +1,5 @@
+export const burgerButtonSelectors = { 
+    allItemsSelector : '#inventory_sidebar_link',
+    aboutSelector : '#about_sidebar_link',
+    resetSelector : '#reset_sidebar_link', 
+}
